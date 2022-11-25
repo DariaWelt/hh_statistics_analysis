6a3ddca311f2b6790c3100ab898a7fabb51c9180
DariaWelt <daria.welt@bk.ru>
2022-11-25 17:16:14 +0100

    Updated styles

--- hh_analyzer/UserApp/UserInterfaceService.py
+++ hh_analyzer/UserApp/UserInterfaceService.py
@@ -1,4 +1,5 @@
 from os import environ as env
+from typing import List, Dict
 
 import dash
 import numpy as np
@@ -60,7 +61,8 @@ class UserInterface(HHService):
     def add_extractor_button(self):
         self._dash_app.layout = html.Div([
             self._dash_app.layout,
-            html.Button('Extract month data', id='extract-data'),
+            html.Button('Extract month data', id='extract-data',
+                        style={'width': '10%', 'height': '10%', 'marginTop': 5, 'font-size': '14px'}),
         ])
 
         @self._dash_app.callback(
@@ -79,14 +81,25 @@ class UserInterface(HHService):
     def add_processor_button(self):
         self._dash_app.layout = html.Div([
             self._dash_app.layout,
-            'Type tool or technology which you want to analyze:',
-            html.Div(dcc.Input(id='input-on-processing-button', type='text')),
-            html.Button('Process data', id='processing-data', n_clicks=0),
-            html.Div(id='processing-button-basic', children='Click to process data for last month'),
+            html.Br(),
+            html.Div(children='List tools or technologies which you want to analyze with ";":'),
+            dcc.Input(id='input-on-processing-button', type='text', placeholder='python; flask;gcc', required=True,
+                      style={'width': '50%'}),
+            html.Div(id='processing-button-data', style={'marginTop': 5, 'width': '50%'}),
+            html.Button('Process data', id='processing-data', n_clicks=0,
+                        style={'width': '10%', 'height': '10%', 'marginTop': 5, 'font-size': '14px'}),
         ])
 
         @self._dash_app.callback(
-            Output('processing-button-basic', 'children'),
+            Output('processing-button-data', 'children'),
+            Input('input-on-processing-button', 'value')
+        )
+        def update_output(value):
+            tools = value.split(';') if value else []
+            return dcc.Dropdown(tools, tools, multi=True, disabled=True)
+
+        @self._dash_app.callback(
+            Output('dashboard', 'children'),
             Input('processing-data', 'n_clicks'),
             State('input-on-processing-button', 'value')
         )
@@ -96,58 +109,54 @@ class UserInterface(HHService):
             self._kafka_producer.flush()
             self._logger.info(f"sent to {self._processing_kafka_theme}: message '{value}'")
             self._waiting_for_processing = True
-            self.wait_for_response()
-            return 'The input value was "{}" and the button has been clicked {} times'.format(
-                value,
-                n_clicks
-            )
+            return self.wait_for_response()
 
     def wait_for_response(self):
         for message in self._kafka_consumer:
             self._logger.info(f"got {message.topic}: message '{message.value}'")
             if message.topic == f'resp_{self._extractor_kafka_theme}':
                 self._waiting_for_extractor = False
-                self._logger.info('got response from extractor')
+                return 1
             if message.topic == f'resp_{self._processing_kafka_theme}':
                 self._waiting_for_processing = False
-                self.update_dashboard()
-                self._logger.info('got response from processing')
+                return self.get_dashboard()
 
     def add_dashboard(self):
-        vac_percentage = go.Figure(data=[go.Pie(values=[self.TEST_DATA_PART, 100-self.TEST_DATA_PART],
+        self._dash_app.layout = html.Div([
+            self._dash_app.layout,
+            html.Div(id='dashboard',
+                     children=self.get_dashboard())
+        ])
+
+    def get_dashboard(self) -> List[html.Div]:
+        self._logger.info(f"called dash updating method..")
+        vac_percentage = go.Figure(data=[go.Pie(values=[self.TEST_DATA_PART, 100 - self.TEST_DATA_PART],
                                                 name="Part of vacancies where technology is required",
                                                 textinfo='none',
                                                 hole=.6,
                                                 marker_colors=['rgb(36, 73, 147)', 'rgb(256, 256, 256)'])])
 
         bar_fig = express.bar(self.TEST_DATA_SALARY, x='technologies', y='with',
-                      barmode='stack', text='technologies')
+                              barmode='stack', text='technologies')
         bar_fig.update_layout(title="Salaries distribution over technologies",
-                           xaxis_title='technology',
-                           yaxis_title='Salary', width=800)
+                              xaxis_title='technology',
+                              yaxis_title='Salary', width=800)
         bar_fig.update_traces(texttemplate='%{text:.2s}', textposition='inside')
 
         correlation_fig = express.imshow([[0.2, 0.1, 0.6, 0.9],
                                           [0.3, 0.6, 0.9, 0.2],
                                           [0.5, 0.2, 0.4, 0.5],
                                           [0.1, 0.1, 0.6, 0.1]], text_auto=True)
-
-        self._dash_app.layout = html.Div([
-            self._dash_app.layout,
+        return [
             html.Div(className='row',  # Define the row element
-                     children=[
-                         html.Div(className='histogramm', children=dcc.Graph(figure=bar_fig),
-                                  style={'width': '69%', 'display': 'inline-block'}),  # Define the left element
-                         html.Div(className='pie chart', children=dcc.Graph(figure=vac_percentage),
-                                  style={'width': '29%', 'display': 'inline-block'})  # Define the right element
-                     ]),
-            html.Div(className='correlation_matrix',
-                     children=dcc.Graph(figure=correlation_fig))
-
-        ])
-
-    def update_dashboard(self):
-        self._logger.info(f"called dash updating method..")
+                    children=[
+                        html.Div(className='histogramm', children=dcc.Graph(figure=bar_fig),
+                                 style={'width': '69%', 'display': 'inline-block'}),
+                        html.Div(className='pie chart', children=dcc.Graph(figure=vac_percentage),
+                                 style={'width': '29%', 'display': 'inline-block'})
+                      ]),
+            html.Div(className='correlation_matrix', children=dcc.Graph(figure=correlation_fig))
+        ]
 
     def _configure(self):
         if self._config_path:
